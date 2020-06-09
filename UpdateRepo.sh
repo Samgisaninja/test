92f73a33a57078a1fe23da9221947a61da3ee62a
Sam Gardner <samgisaninja@unc0ver.dev>
2020-06-09 16:15:39 -0500

    successioncli

--- UpdateRepo.sh
+++ UpdateRepo.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 rm Packages*
-dpkg-scanpackages -m ./debs/ /dev/null >Packages
+dpkg-scanpackages -m ./debs/ > Packages
+cp Packages Packages_
 bzip2 Packages
+mv Packages_ Packages
+
